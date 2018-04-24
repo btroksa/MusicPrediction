2e9573aeac10ac6a768f39d5d78648f701d095c5
dcdennis <devincdennis@gmail.com>
2018-04-24 12:00:19 -0600

    Closes RDD filtering by  genre annotation

--- src/main/scala/FilterGenre.scala
+++ src/main/scala/FilterGenre.scala
@@ -1,17 +1,46 @@
 import java.io._
+import scala.io.Source;
 
 import org.apache.spark.HashPartitioner
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.rdd.RDD
 
-object FilterGenre {
+/** Genres: Rock, Rap, Latin, Jazz, Electronic, Pop, Metal, Country, Reggae, RnB, Blues, Folk, Punk, World, New Age **/
 
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder.appName("tester").master("local").getOrCreate()
-    val textFile = spark.read.textFile("MillionSongSubset/data/A/A/A/TRAAAAW128F429D538.h5").rdd
-    textFile.foreach(println)
+  class evalGenreAnnotations extends PartialFunction [String, String] with Serializable{
+
+    var filter = ""
+
+    val validate: PartialFunction[Array[String], String] = {
+      case lines if lines.length == 2 && lines(1).equals(this.filter) => lines(0)
+    }
+
+    override def apply(log: String): String = {
+      validate(log.split("\t"))
+    }
+
+    override def isDefinedAt(x: String): Boolean = {
+      validate.isDefinedAt(x.split("\t"))
+    }
 
   }
 
+object FilterGenre {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder.appName("tester").master("local").getOrCreate()
+    val textFile = spark.read.textFile("/Users/devindennis1/Documents/CS455/MusicPredictionDataSet/msd_tagtraum_cd2c.cls").rdd
+
+    val validator = new evalGenreAnnotations
+    validator.filter = args(0)
+    println("Filter By Genre: " + validator.filter + "\n")
+
+    val trackIDs = textFile.collect(validator)
+    trackIDs.foreach(println)
+
+    // create an empty map -- var map = scala.collection.mutable.Map[String, String]()
+
+  }
 
 }

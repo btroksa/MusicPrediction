da90e71d5994994658b7e2a1100dccaa39dc60d7
dcdennis <devincdennis@gmail.com>
2018-04-24 23:59:24 -0600

    genre midi filtration working

--- src/main/scala/FilterGenre.scala
+++ src/main/scala/FilterGenre.scala
@@ -15,31 +15,53 @@ class evalGenreAnnotations extends PartialFunction [String, String] with Seriali
     case lines if lines.length == 2 && lines(1).equals(this.filter) => lines(0)
   }
 
-  override def apply(log: String): String = {
-    validate(log.split("\t"))
+  override def apply(line: String): String = {
+    validate(line.split("\t"))
   }
 
-  override def isDefinedAt(x: String): Boolean = {
-    validate.isDefinedAt(x.split("\t"))
+  override def isDefinedAt(line: String): Boolean = {
+    validate.isDefinedAt(line.split("\t"))
   }
 
 }
 
 object FilterGenre {
 
+  var genreSet = scala.collection.mutable.HashSet[String]()
+
+
+  def traverse(dir: File): Unit =
+    dir.listFiles foreach {
+        f =>  val item = f.toString.substring(f.toString.lastIndexOf("/") + 1, f.toString.length)
+              if (f.isDirectory && item.length == 18 && this.genreSet.contains(item)) traverse(f)
+              else if (f.isDirectory && item.length == 18) doNothing(f)
+              else if(item.equals(".DS_Store")) doNothing(f)
+              else if (f.isDirectory && item.length() == 1) traverse(f)
+              else process(f)
+    }
+
+  def doNothing(File: File) = {}
+
+  def process(File: File) ={
+    // TODO: UPDATE TO WRITE ALL PROCESSED FILES
+    println("process: " + File)
+  }
+
+  /** sbt shell: run <Genre> **/
   def main(args: Array[String]): Unit = {
 
-    val path = "/Users/devindennis1/Documents/CS455/MusicPredictionDataSet/msd_tagtraum_cd2c.cls"
+    val path = "/Users/devindennis1/Documents/CS455/MusicPredictionDataSet/msd_tagtraum_cd2c.cls" // TODO: UPDATE FILE PATH
     val validator = new evalGenreAnnotations
     validator.filter = args(0)
 
+    println("\nFilter By Genre: " + validator.filter + "\n")
+
     val spark = SparkSession.builder.appName("tester").master("local").getOrCreate()
     val trackIDs = spark.read.textFile(path).rdd.collect(validator)
+    trackIDs.foreach(genreSet.add(_))
 
-    println("\nFilter By Genre: " + validator.filter + "\n")
-    trackIDs.foreach(println)
-
-    // create an empty map -- var map = scala.collection.mutable.Map[String, String]()
+    val midis = "/Users/devindennis1/Documents/CS455/MusicPredictionDataSet/lmd_matched"          // TODO: UPDATE FILE PATH
+    traverse(new File(midis))
 
   }
 

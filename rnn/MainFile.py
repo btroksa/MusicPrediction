d06213195b65e12e2b04b8068fdd90eadd4701f6
Blake Troksa <blake.troksa@gmail.com>
2018-05-01 02:39:53 -0600

    changes

--- rnn/MainFile.py
+++ rnn/MainFile.py
@@ -7,7 +7,7 @@ def Begin(f):
     # Read in Midi organize data
     try:
         import Preprocess
-        file = str(f)
+        file = "file://~/"+str(f)
         print(file)
         Instruments = Preprocess.getInstruments(file)
         dictOfNotes = Preprocess.getNotes(Instruments)
